874f570f60b77626016990f9fa5c1506805ac0f8
Pierrinho <megaptery@gmail.com>
2013-11-08 13:35:11 +0100

    Release 2.0

--- README.md
+++ README.md
@@ -1,7 +1,56 @@
 # Allofthelights.js
-jQuery plugin to turn off the light and enjoy your videos, try it !
+Allofthelights.js is a jQuery plugin to turn off the light and enjoy your videos, try it !
+http://www.megaptery.com/allofthelights/ - Pierre VION
 
-http://megaptery.com/allofthelights/
+## Features
+- Multiple videos iframe
+- Responsive video
+- Support of lot of players
+- Switch button entirely customizable
+- Lightweight (3.4Ko minified)
+- Callback functions
+- Control animation speed
+- Custom background (color, opacity)
+- Modern browsers support
+- Requires jQuery 1.7+
 
-Allofthelights.js is a jQuery plugin that allows to switch off the light, for your videos to stand out. Give it a try by clicking the switch next to the video. Bear in mind this is a first version.
- 
+## Examples
+
+- [Sample demo](http://www.megaptery.com/allofthelights/demo/index.html)
+- [Page plugin](http://www.megaptery.com/allofthelights/)
+
+
+## Options
+
+### color: *'#000000'*
+Defines the background color.
+
+### opacity: *'0.9'*
+Defines the background opacity.
+
+### z_index: *'10'*
+Defines the background z-index.
+
+### switch_selector: *'switch'*
+Defines the switch button CSS id or class.
+
+### delay_turn_on: *400*
+Defines animation time to turn on the light.
+
+### delay_turn_off: *400*
+Defines animation time to turn off the light.
+
+### scrolling: *true*
+Allows disable scrolling when the light is on if value is false.
+
+### is_responsive: *true*
+Enables responsive video.
+
+### custom_player: *null*
+Allows to apply responsive on a custom player. Specify your video vendor selector. Only available when is_responsive = true.
+
+### callback_turn_on: *function(){}*
+Callback function when the light goes on.
+
+### callback_turn_off: *function(){}*
+Callback function when the light goes off.
--- jquery.allofthelights.js
+++ jquery.allofthelights.js
@@ -1,181 +1,207 @@
 /*
 * Allofthelights.js
-* Version: 1.0
-* http://www.megaptery.com/allofthelights/
+* Version: 2.0
+* Description: jQuery plugin to turn off the light and enjoy your videos !
+* Licence: MIT
+* Infos: http://www.megaptery.com/allofthelights/
+* Author: Pierre VION - http://www.pierrevion.fr/
 */
 
 (function ($) {
+
     $.fn.allofthelights = function (options) {
 
         var defaults = {
-            'color': '#000000',
-            'opacity': '0.9',
-            'switch_id': 'switch',
-			'animation': 'fade',
-			'delay_turn_on': 400,
-			'delay_turn_off': 400,
-			'scrolling': true,
-			'clickable_bg': false,
-			'is_responsive': true,
-			'z_index': '10',
-			'custom_player': null,
+            color: 'black',
+            opacity: '0.9',
+            z_index: '10',
+            switch_selector: 'switch',
+			delay_turn_on: 400,
+			delay_turn_off: 400,
+			scrolling: true,
+			is_responsive: true,
+			custom_player: null
         };
 
-	var options = $.extend(defaults, options);
+		var options = $.extend(defaults, options);
+		var $body = $('body');
+		var $elements = [];
+		var switch_selector = '#'+options.switch_selector+', .'+options.switch_selector;
 
-        return this.each(function () {
+		this.each(function() {
+			var $this = $(this);
+			$elements.push($this);
+		});
 
-			var $this		= $(this),
-			first_click		= true,
-			offset			= null,
-			height			= null,
-			width			= null,
-			width_window	= null,
-			height_window	= null,
-			button			= null,
-			switch_off		= '#'+options.switch_id+'_off';
-				
-			if (options.is_responsive) {
-				var selectors = [
-					"iframe[src*='player.vimeo.com']",
-					"iframe[src*='www.youtube.com']",
-					"iframe[src*='www.dailymotion.com']",
-					"iframe[src*='www.kickstarter.com']",
-					"object",
-					"embed"
-				];
-				
-				if (options.custom_player) {
-					selectors.push(options.custom_player);
-				}
+		if (options.is_responsive) {
 
-				var $allVideos = $(this).parent().find(selectors.join(','));
-				
-				var style = '&shy;<style type="text/css">         \
-				  .fluid_width_video_wrapper {        \
-					 width: 100%;                     \
-					 position: relative;              \
-					 padding: 0;                      \
-				  }                                   \
-													  \
-				  .fluid_width_video_wrapper iframe,  \
-				  .ffluid_width_video_wrapper object,  \
-				  .fluid_width_video_wrapper embed {  \
-					 position: absolute;              \
-					 top: 0;                          \
-					 left: 0;                         \
-					 width: 100%;                     \
-					 height: 100%;                    \
-				  }                                   \
-				</style>';
-				$('body').append(style);
-
-				$allVideos.each(function(){
-					var $this = $(this);
-					if (this.tagName.toLowerCase() == 'embed' && $this.parent('object').length || $this.parent('.fluid_width_video_wrapper').length) { return; }
-					var height = ( this.tagName.toLowerCase() == 'object' || $this.attr('height') ) ? $this.attr('height') : $this.height(),
-					width = $this.attr('width') ? $this.attr('width') : $this.width(),
-					aspectRatio = height / width;
-					if(!$this.attr('id')){
-						var videoID = 'fitvid' + Math.floor(Math.random()*999999);
-						$this.attr('id', videoID);
-					}
-					$this.wrap('<div class="fluid_width_video_wrapper"></div>').parent('.fluid_width_video_wrapper').css('padding-top', (aspectRatio * 100)+"%");
-					$this.removeAttr('height').removeAttr('width');
-				});
-			}
-
-			if (options.animation == 'none') {
-				options.delay_turn_on = 0;
-				options.delay_turn_off = 0;
-			}
-
-			if (options.clickable_bg) {
-				switch_off += ', div.allofthelights_bg';
-			}
-
-			$('body').on('click', switch_off, function() {
-				variables();
-				update();
-				$('div.allofthelights_bg').fadeOut(+options.delay_turn_off);
-				$('#'+options.switch_id+'_off').fadeOut(0);
-				if (!options.scrolling) {
-					$('body').css('overflow', 'auto');
-				}
-			}).on('click', '#'+options.switch_id, function() {
-				variables();
-				if (first_click) {
-					first_click = false;
-					var html = "<style type='text/css'>.allofthelights_bg {display:none;position:absolute;background:"+options.color+";z-index:"+options.z_index+";}</style>" + "<div id='"+options.switch_id+"_off' style='display:none;position:absolute;top:"+button.top+"px;left:"+button.left+"px;'></div>";
-					var i 	 = 0;
-					for ( i = 1 ; i <= 4 ; ++i ) {
-						html += "<div id='allofthelights_bg"+i+"' class='allofthelights_bg'></div>"
-					}
-					$('body').append(html);
-					$('.allofthelights_bg').css('opacity',+options.opacity);
-					update();
-				}
-				$('#'+options.switch_id+'_off').fadeIn(0);
-				$('div.allofthelights_bg').fadeIn(+options.delay_turn_on);
-				
-				if (!options.scrolling) {
-					$('body').css('overflow', 'hidden');
-				}
-			});
-			$(window).on('resize', function() {
-				var display = false;
-				if ( $('div.allofthelights_bg').is(':visible') ) {
-					display = true;
-					$('div.allofthelights_bg').hide();
-				}
-				variables();
-				update();
-				if (display) {
-					$('div.allofthelights_bg').show();
-				}		
-			});
+			var selectors = [
+				"iframe[src*='player.vimeo.com']",
+				"iframe[src*='www.youtube.com']",
+				"iframe[src*='www.dailymotion.com']",
+				"iframe[src*='www.kickstarter.com']",
+				"object",
+				"embed"
+			];
 			
-			function variables() {            
-				offset 		= $this.offset();
-				height 		= $this.height();
-				width 		= $this.width();
-				width_window 	= $(document).width();
-				height_window 	= $(document).height();
-				button 		= $('#'+options.switch_id).offset();
+			if (options.custom_player) {
+				selectors.push(options.custom_player);
+			}
+
+			var $all_videos = $(this).parent().find(selectors.join(','));
+			
+			var style = 
+			'<style type="text/css">				\
+				.fluid_width_video_wrapper {		\
+					width: 100%;					\
+					position: relative;				\
+					padding: 0;						\
+				}									\
+				.fluid_width_video_wrapper iframe,	\
+				.fluid_width_video_wrapper object,	\
+				.fluid_width_video_wrapper embed {	\
+					position: absolute;				\
+					top: 0;							\
+					left: 0;						\
+					width: 100%;					\
+					height: 100%;					\
+				}									\
+			</style>';
+
+			$body.append(style);
+
+			$all_videos.each(function(){
+				var $this = $(this);
+				if (this.tagName.toLowerCase() == 'embed' && $this.parent('object').length || $this.parent('.fluid_width_video_wrapper').length) { return; }
+				var height = ( this.tagName.toLowerCase() == 'object' || $this.attr('height') ) ? $this.attr('height') : $this.height(),
+				width = $this.attr('width') ? $this.attr('width') : $this.width(),
+				aspectRatio = height / width;
+				if(!$this.attr('id')){
+					var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+					$this.attr('id', videoID);
+				}
+				$this.wrap('<div class="fluid_width_video_wrapper"></div>').parent('.fluid_width_video_wrapper').css('padding-top', (aspectRatio * 100)+"%");
+				$this.removeAttr('height').removeAttr('width');
+			});
+		}
+
+		var style = 
+		'<style type="text/css">				\
+			.allofthelights_bg {				\
+				position: absolute;				\
+				display: none;					\
+				background: '+options.color+';	\
+				opacity: '+options.opacity+';	\
+				z-index: '+options.z_index+';	\
+			}									\
+		</style>';
+		$body.append(style);
+
+		$body.on('click', '.allofthelights_bg', function() {
+			var callback_turn_on = true;
+			$('.allofthelights_bg').fadeOut(+options.delay_turn_on, function() {
+				if (!options.scrolling) {
+					$body.css('overflow', 'auto');
+				}
+				if (callback_turn_on && $.isFunction(options.callback_turn_on)) {
+					options.callback_turn_on.call(this);
+					callback_turn_on = false;
+				}
+			});			
+		}).on('click', switch_selector, function() {
+			if (!options.scrolling) {
+				$body.css('overflow', 'hidden');
+			}
+			calculating();
+		});
+
+		$(window).on('resize', function() {
+			if ($('div.allofthelights_bg').is(':visible')) {
+				calculating();
+			}
+		});
+
+		function calculating() {
+
+			var x = [];
+			var y = [];
+			var id = 1;
+			var callback_turn_off = true;
+
+			x.push(0.0);
+			x.push($(document).width());
+			y.push(0.0);
+			y.push($(document).height());
+					
+			for (var i = 0 ; i < $elements.length ; ++i) {
+				var $video = $elements[i];
+				var offset = $video.offset();
+				var height = $video.height();
+				var width = $video.width();
+				x.push(offset.left); x.push(offset.left + width);
+				y.push(offset.top); y.push(offset.top + height);
 			}
 			
-			function update() {
-				$('#'+options.switch_id+'_off').css({
-					'top': button.top,
-					'left': button.left
-				});
-				$('#allofthelights_bg1').css({
-					'top': '0px',
-					'height': offset.top,
-					'left': '0px',
-					'right': '0px'
-				});				
-				$('#allofthelights_bg2').css({
-					'height': height,
-					'top': offset.top,
-					'left': '0px',
-					'right': (width_window - offset.left)
-				});
-				$('#allofthelights_bg3').css({
-					'height': height,
-					'top': offset.top,
-					'right': '0px',
-					'bottom': '0px',
-					'left': (offset.left+width)
-				});
-				$('#allofthelights_bg4').css({
-					'height': (height_window-(offset.top+height)),
-					'top': (offset.top+height),
-					'bottom': '0px',
-					'left': '0px',
-					'right': '0px'
-				});	
+			x.sort(function(a,b){return a-b});
+			y.sort(function(a,b){return a-b});
+			
+			for (var i = 0 ; i < x.length - 1 ; ++i) {
+
+				for (var j = 0 ; j < y.length - 1 ; ++j) {
+
+					var ax = x[i];
+					var ay = y[j]; 
+					var bx = x[i + 1];
+					var by = y[j + 1];                      
+					var to_display = true;
+					
+					for (var k = 0 ; k < $elements.length ; ++k) {
+
+						var $video = $elements[k];
+						var offset = $video.offset();
+						var height = $video.height();
+						var width = $video.width();
+						var vax = offset.left;
+						var vay = offset.top; 
+						var vbx = offset.left + width;
+						var vby = offset.top + height;  
+						
+						if (ax >= vax && ay >= vay && bx <= vbx && by <= vby) {
+							to_display = false;
+							break;
+						}					
+					}
+						
+					if (to_display) {
+
+						if (!$('#allofthelights_bg' + id).length > 0) {
+							var div = "<div id='allofthelights_bg"+ id +"' class='allofthelights_bg'></div>";
+							$body.append(div);
+						}
+						
+						$('#allofthelights_bg' + id).css({
+							top: ay,
+							left: ax,
+							right: '0',
+							height: by - ay,
+							width: bx - ax
+						});
+
+						id++;
+
+					}
+				}
 			}
-        });
-    };
+
+			if (!$('div.allofthelights_bg').is(':visible')) {
+				$('.allofthelights_bg').fadeIn(+options.delay_turn_off, function() {
+					if (callback_turn_off && $.isFunction(options.callback_turn_off)) {
+						options.callback_turn_off.call(this);
+						callback_turn_off = false;
+					}
+				});
+			}
+			
+		}
+	}
 })(jQuery);
